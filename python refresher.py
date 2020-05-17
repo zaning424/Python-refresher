1013e5308bfabd662a752285099a87821f404936
Zaning <61484146+zaning424@users.noreply.github.com>
2020-05-17 05:46:52 -0400

    add if__name__

--- python refresher.py
+++ python refresher.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 x = np.arange(0, 2*np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('sin(x)')
-plt.show()
 
+if __name__ == "__main__":
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('sin(x)')
+    plt.show()
 
-plt.plot(x, z)
-plt.xlabel('x')
-plt.ylabel('cos(x)')
-plt.show()
+    plt.plot(x, z)
+    plt.xlabel('x')
+    plt.ylabel('cos(x)')
+    plt.show()
 
